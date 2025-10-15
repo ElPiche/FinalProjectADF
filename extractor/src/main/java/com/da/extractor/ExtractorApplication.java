3e087f9f26051829f4879bf9e473920fbfb47141
BraG29-santos-code <bgranero@santoscode.com>
2025-10-15 19:17:20 -0300

    Configuraciones de Docker para buildear y correr el extractor

--- extractor/src/main/java/com/da/extractor/ExtractorApplication.java
+++ extractor/src/main/java/com/da/extractor/ExtractorApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExtractorApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ExtractorApplication.class, args);
+
+        IO.println("Extractor Service Started...");
     }
 
 }
