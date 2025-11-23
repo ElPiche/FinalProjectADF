49fe304bbfca3702f652a02a2b2e05123f759cf7
ShodytheWolf <martin.antunez5@gmail.com>
2025-11-23 17:12:54 -0300

    fixed stash merge

--- MotorDA/Dispatcher/DADispatcher.py
+++ MotorDA/Dispatcher/DADispatcher.py
@@ -1,17 +1,16 @@
-<<<<<<< Updated upstream
-import json
-=======
->>>>>>> Stashed changes
 import threading
 import time
 import traceback
+from concurrent.futures import ProcessPoolExecutor
+from queue import Queue
+
 import requests
 from datetime import datetime, timezone, timedelta
 from bson import json_util
 from pymongo import MongoClient
 import pandas as pd
 from typing import Dict, Any, List, Optional
-from elasticsearch import Elasticsearch, helpers
+from elasticsearch import Elasticsearch
 from dataclasses import dataclass
 
 from pymongo.errors import PyMongoError
@@ -27,25 +26,13 @@ from ZScore.standalone_da_algorithm_z_score import (
     get_closest_bucket
 )
 
-
-@dataclass
-class ZScore:
-    def __init__(self, train_window: int, train_from: str, train_to: str, threshold: float, observed_values: Dict[str, pd.DataFrame]):
-        self.train_window = train_window  # in minutes
-        self.threshold = threshold
-        self.train_from = train_from
-        self.train_to = train_to
-        self.observed_values = observed_values
-
-    def __repr__(self):
-        return f"ZScore(window={self.train_window}min, threshold={self.threshold}, metrics={list(self.observed_values.keys())})"
-
-
 # Dispatcher: Tiene como objetivo recibir el documento de configuración desde MongoDB y despachar la ejecución del algoritmo correspondiente.
 # Tiene que en base al documento de configuración, identificar qué algoritmo se debe ejecutar y llamar a la función correspondiente,
 # pasándole los parámetros necesarios. Y guardar los resultados en ElasticSearch.
 # En la versión actual también consultaría el mongoDB que contiene la data de documentación para leer la metadata y saber si esta entrenando o no jeje.
 
+
+# ----------------------------------- CONSTANT DECLARATION -------------------------------------------------------------------
 # conexión a MongoDB
 MONGO_KB_URL = "mongodb://admin:1q2w3E%2A@mongodb:27017/?authSource=admin"
 MONGO_DB_NAME = "anomaly_detection"
@@ -61,8 +48,24 @@ ANOMALIES_INSIGHT_URL = "http://anomalies-insights:8081/api/"
 ES_HOST = "http://elasticsearch-anomalies:9200"
 ES_INDEX = "test_logs"
 
+QUEUE_MAX_SIZE = 5000
 
 elastic_client = Elasticsearch(ES_HOST)
+
+@dataclass
+class ZScore:
+    def __init__(self, train_window: int, train_from: str, train_to: str, threshold: float, observed_values: Dict[str, pd.DataFrame]):
+        self.train_window = train_window  # in minutes
+        self.threshold = threshold
+        self.train_from = train_from
+        self.train_to = train_to
+        self.observed_values = observed_values
+
+    def __repr__(self):
+        return f"ZScore(window={self.train_window}min, threshold={self.threshold}, metrics={list(self.observed_values.keys())})"
+
+
+
 
 
 @dataclass
@@ -266,30 +269,6 @@ def delete_series( config: Config):
     result =da_client[MONGO_DB_NAME][SERIES_COLLECTION_NAME].delete_many(query)
     print(result)
 
-def run_arma(config_block: Dict[str, Any]):
-    params = (config_block.get("daAlgParameters") or {}).get("arma", [])
-    for p in params:
-        print(
-            f"▶ ARMA p={p.get('p')} d={p.get('d')} q={p.get('q')} observedValue={p.get('observedValue')}")
-    # implementación...
-
-
-def run_kmeans(config_block: Dict[str, Any]):
-    params = (config_block.get("daAlgParameters") or {}).get("kmeans", [])
-    for p in params:
-        print(
-            f"▶ KMEANS nClusters={p.get('nClusters')} observedValue={p.get('observedValue')}")
-    # implementación...
-
-
-def run_iforest(config_block: Dict[str, Any]):
-    params = (config_block.get("daAlgParameters") or {}).get("iforest", [])
-    for p in params:
-        print(
-            f"▶ IFOREST nEstimators={p.get('nEstimators')} contamination={p.get('contamination')} observedValue={p.get('observedValue')}")
-    # implementación...
-
-
 def fetch_series_data_with_aggregation(
     config: Config, algorithm_to_execute: Algorithm
 ) -> Dict[str, pd.DataFrame]:
@@ -464,8 +443,6 @@ def watch_kb_changes(kb_client):
                         print(
                             f"\033[31m Someone inserted data into: {TRAINING_COLLECTION_NAME} \033[0m")
 
-                        # TODO: add delete if exists for the series result
-
                         # de aquí extraemos los datos de las operativas que vayamos a llamar en formato de JSON
                         latest_series_config = ExtractLatestConfigurationKB(kb_client)
 
@@ -474,11 +451,11 @@ def watch_kb_changes(kb_client):
 
                         # now we go thru each algorithm call we extracted, and try to execute training on them
                         config.execute_algos()
-                        # TODO: delete series once they are trained
 
         except PyMongoError as e:
             print(f"[watch_kb_changes] Mongo error: {e}, reconnecting in 5s...")
             time.sleep(5)
+
         except Exception as e:
             print(f"[watch_kb_changes] Unexpected error: {e}")
             traceback.print_exc()
@@ -486,15 +463,9 @@ def watch_kb_changes(kb_client):
 
 
 
-<<<<<<< Updated upstream
-# TODO: test how robust it is this with a lot of different datapoints sent at the same time
-# TODO: check how change stream works with threads
-def watch_detection_changes(kb_client):
-=======
 # TODO: test how robust it is this with a lot of different datapoints sent at the same time -> pretty robust, we tried it with 10400 entries
 # TODO: check how change stream works with threads -> works synchronously, fetches one at a time
 def watch_detection_changes(kb_client, workers: ProcessPoolExecutor, data_to_detect: Queue):
->>>>>>> Stashed changes
 
     while True:
         try:
@@ -503,6 +474,7 @@ def watch_detection_changes(kb_client, workers: ProcessPoolExecutor, data_to_det
             ]) as stream:
 
                 for change in stream:
+
                     print(f"something happened on: {SERIES_COLLECTION_NAME}")
 
                     if change.get("operationType") == "insert":
@@ -511,92 +483,7 @@ def watch_detection_changes(kb_client, workers: ProcessPoolExecutor, data_to_det
 
                         serie_to_detect = change.get("fullDocument")
 
-<<<<<<< Updated upstream
-                        print(f"I am printing serie_to_detect: {serie_to_detect}")
-
-                        pipeline = [{'$match':
-                                    {'kb_id': serie_to_detect["metadata"]["kbId"],
-                                     'dimension': serie_to_detect["metadata"]["dim"]}
-                                     }]
-
-                        result = kb_client[MONGO_DB_NAME][SERIES_RESULT_COLLECTION_NAME].aggregate(
-                            pipeline)
-
-                        training_result = next(result, None)
-                        #print(f"I am priting the result of training:  {training_result}")
-
-                        # 2) flatten nested fields (metadata -> metadata.kbId etc.)
-                        df = pd.json_normalize(serie_to_detect)
-
-                        # optional: rename for convenience
-                        df = df.rename(columns={
-                            "metadata.kbId": "kbId",
-                            "metadata.dim": "dim",
-                            "metadata.mode": "mode"
-                        })
-                        # TODO: delete series once they are detected
-
-                        # 3) make _id string (pandas doesn't like ObjectId)
-                        if "_id" in df.columns:
-                            df["_id"] = df["_id"].astype(str)
-
-                        # 4) ensure timestamp is datetime dtype
-                        if "timestamp" in df.columns:
-                            df["timestamp"] = pd.to_datetime(df["timestamp"])
-
-                        if training_result["work_day_enabled?"]:
-
-                            #we add a boolean to know if it's a workday or not
-                            df["is_workday"] = df["timestamp"].dt.dayofweek < 5
-
-                            print("I am detecting Z score with work day enabled")
-                            bucket_value = get_closest_bucket(df, training_result, training_result["time_window"])
-
-                            anomalies = anomaly_detection_workdayful(
-                                df, training_result, training_result["time_window"], bucket_value)
-
-                        else:
-
-                            bucket_value = get_closest_bucket(df, training_result, training_result["time_window"])
-                            print("I am detecting Z score with work day disabled")
-
-                            anomalies = anomaly_detection_workdayless(
-                                df, training_result, training_result["time_window"], bucket_value)
-
-                        if anomalies[0].get("is_anomaly"):
-                            print("=========================================================================================================================")
-                            print(f"\033[31m anomaly detected: {anomalies} \033[0m")
-                            print(
-                                "=========================================================================================================================")
-
-                            url_post = ANOMALIES_INSIGHT_URL + "insights/insertDocument/" + serie_to_detect["metadata"]["kbId"]
-                            headers = {'Accept': 'application/json', "Content-Type": "application/json"}
-
-                            # convert values that might be datetimes into ISO strings
-                            ts = anomalies[0].get("timestamp")
-
-                            if isinstance(ts, datetime):
-                                # send UTC ISO 8601 string (add 'Z' or include tzinfo)
-                                ts = ts.astimezone(timezone.utc).isoformat().replace("+00:00", "Z")
-
-                            processed_data = {
-                                'algorithm': 'Z Score',
-                                'metric': serie_to_detect["metadata"]["dim"],
-                                'text': "Anomaly detected",
-                                'timestamp': ts,
-                                'value': anomalies[0].get("value"),
-                                'created_at': datetime.now(timezone.utc).replace(tzinfo=timezone.utc).isoformat().replace(
-                                    "+00:00", "Z")
-                            }
-
-                            r = requests.post(url_post, json=processed_data, headers=headers)
-
-                            print(r.status_code, r.text)
-                            # for debugging, you can inspect what was actually sent:
-                            print("REQUEST BODY:", r.request.body)  # will be a JSON string
-=======
                         workers.submit(detect_z_score, serie_to_detect)
->>>>>>> Stashed changes
 
         except PyMongoError as e:
             print(f"[watch_detection_changes] Mongo error: {e}, reconnecting in 5s...")
@@ -608,8 +495,6 @@ def watch_detection_changes(kb_client, workers: ProcessPoolExecutor, data_to_det
             traceback.print_exc()
             time.sleep(5)
 
-<<<<<<< Updated upstream
-=======
 
 def detect_z_score(serie_to_detect):
 
@@ -699,7 +584,6 @@ def detect_z_score(serie_to_detect):
         requests.post(url_post, json=processed_data, headers=headers)
 
 
->>>>>>> Stashed changes
 def restartable_thread(target, *args, delay=5):
     """Runs the given target in a loop, restarting if it crashes."""
     while True:
@@ -715,13 +599,10 @@ def main():
     # Esto arma la conexión a MongoDB
     kb_client = CreateConnectionToKB()
 
-<<<<<<< Updated upstream
-=======
     data_to_detect: Queue = Queue(maxsize=QUEUE_MAX_SIZE)
 
     workers : ProcessPoolExecutor = ProcessPoolExecutor()
 
->>>>>>> Stashed changes
     # Start watcher in its own thread
     training_watcher = threading.Thread(
         target=restartable_thread,
@@ -730,7 +611,7 @@ def main():
 
     detection_watcher = threading.Thread(
         target=restartable_thread,
-        args=(watch_detection_changes,kb_client,),
+        args=(watch_detection_changes,kb_client, workers, data_to_detect),
         daemon=True
     )
 
